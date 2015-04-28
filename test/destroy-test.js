d34eb8caef6b22867765b2b1e85d0a82eb3ea2f0
Lars-Magnus Skog <ralphtheninja@riseup.net>
2015-04-28 22:31:01 +0200

    rename make-test.js to make.js (it is not a test file)

--- test/destroy-test.js
+++ test/destroy-test.js
@@ -5,7 +5,7 @@ const test         = require('tape')
     , readfiletree = require('readfiletree')
     , testCommon   = require('abstract-leveldown/testCommon')
     , leveldown    = require('../')
-    , makeTest     = require('./make-test')
+    , makeTest     = require('./make')
 
 test('test argument-less destroy() throws', function (t) {
   t.throws(
